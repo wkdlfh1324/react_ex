579a7ae33bd63b17c14932b2faa17be1cbc36daf
wkdlfh1324 <wkdlfh1324@naver.com>
2020-01-03 11:26:24 +0900

    200103

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HelloBox from './components/HelloBox';
+import CountComponent from './components/day1/CountComponent';
+import CountHookComponent from './components/day1/CountHookComponent';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <HelloBox></HelloBox>
+        <CountHookComponent></CountHookComponent>
+        {/* <HelloBox></HelloBox> */}
         <a
           className="App-link"
           href="https://reactjs.org"
